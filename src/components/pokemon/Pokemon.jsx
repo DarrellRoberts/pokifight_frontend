31f6e6b24926e74826bb3e20b87c834c195d8fcc
DarrellRoberts <mrdarrellroberts@gmail.com>
2023-10-06 16:28:42 +0200

    fixed coin issue

--- src/components/pokemon/Pokemon.jsx
+++ src/components/pokemon/Pokemon.jsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import SinglePokemon from "./SinglePokemon";
-import Gamecoin from "../../assets/gamecoin.wav"
-import { Howl } from "howler";
 import Randomiser from "./Randomiser";
 import PokemonView from "./Pokemonview";
 
@@ -28,20 +26,12 @@ export default function Pokemon({ searchBar }) {
   useEffect(() => {
     fetchData();
   }, [searchBar]);
-  
-  const playSound = () => {
-    const sound = new Howl({
-        src: [Gamecoin],
-        volume: 0.15
-    });
-    sound.play();
-}
   return (
     <div style={{ backgroundColor: "black" }}>
       <Randomiser pokemon={pokemon}/>
       {pokemon.map((p, index) => (
         <div style={{ display: "inline-flex", margin: "20px" }} key={index}>
-          <PokemonView onMouseEnter={playSound}  p={p} />
+          <PokemonView  p={p} />
         </div>
       ))}
     </div>
--- src/components/pokemon/Pokemonview.jsx
+++ src/components/pokemon/Pokemonview.jsx
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom";
 import SinglePokemon from "./SinglePokemon";
 import { Card } from "antd";
+import Gamecoin from "../../assets/gamecoin.wav"
+import { Howl } from "howler";
 
 function PokemonView({ p }) {
   const { Meta } = Card;
+
+  const playSound = () => {
+    const sound = new Howl({
+        src: [Gamecoin],
+        volume: 0.15
+    });
+    sound.play();
+}  
   return (
     <div className="pokeContainer" style={{ display: "inline-flex", margin: "20px" }}>
       <Link to={`/pokemon/${p.id}`} element={<SinglePokemon />}>
         <Card
+          onMouseEnter={playSound}
           hoverable
           style={{ width: 220 }}
           cover={
