47a508fee23b5285b564d23308e2b16fac19ea76
Ehsan Shahdoust <ehsan.shahdoust@gmail.com>
2023-10-06 14:49:43 +0200

    Added the search bar

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,20 @@
-
 export default function Homepage() {
-    return(
-        <>
-        
-        <div className="homeContainer" style={{display: "flex", height: "100vh", alignItems:"center", justifyContent: "center", flexDirection: "column"}}>
-        <h1 style={{color: "#FFF", letterSpacing: "20px", fontSize: "5rem"}}>Pokifight</h1>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div
+        className="homeContainer"
+        style={{
+          display: "flex",
+          height: "100vh",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1 style={{ color: "#FFF", letterSpacing: "20px", fontSize: "5rem" }}>
+          Pokifight
+        </h1>
+      </div>
+    </>
+  );
 }
--- src/components/pokemon/SinglePokemon.jsx
+++ src/components/pokemon/SinglePokemon.jsx
@@ -1,44 +1,76 @@
-import { Link, useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import PokemonDetail from "./PokemonDetail"
+import PokemonDetail from "./PokemonDetail";
 import { Card } from "antd";
 
 export default function SinglePokemon() {
-const [pokemon, setPokemon] = useState([])
-const { id } = useParams();
-const { Meta } = Card;
-const fetchData = async () => {
-        const data = await fetch(`https://pokemon-backend-ydlf.onrender.com/api/pokemon/${id}`)
-        const res = await data.json();
-        setPokemon(res);
-        }
-  
+  const [pokemon, setPokemon] = useState([]);
+  const { id } = useParams();
+  const { Meta } = Card;
+  const fetchData = async () => {
+    const data = await fetch(
+      `https://pokemon-backend-ydlf.onrender.com/api/pokemon/${id}`
+    );
+    const res = await data.json();
+    setPokemon(res);
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
-console.log(pokemon);
+  console.log(pokemon);
   if (pokemon) {
     console.log(pokemon?.name?.english);
   } else {
     console.log("Pokemon object is undefined");
   }
-    return (
-        <div style={{backgroundColor: "black"}}>
-        {pokemon.name ? (
-          <div style={{backgroundColor: "black", marginLeft: "5%", padding: "20px"}}>
-          <h1>{pokemon?.name?.english}</h1>
-          <Card 
-        hoverable
-        style={{ width: 220 }}
-    cover={<img alt={pokemon.name.english} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} />}
+  return (
+    <div style={{ backgroundColor: "black" }}>
+      {pokemon.name ? (
+        <div
+          style={{
+            backgroundColor: "black",
+            marginLeft: "5%",
+            padding: "20px",
+          }}
         >
-        <Meta title={pokemon.name.english} description="" />
-        <br/><Link to={`/pokemon/${id}/base`} element={<PokemonDetail name = {pokemon?.name?.english} />}>Stats</Link>
-          <br/><Link to={`/pokemon/${id}/type`} element={<PokemonDetail name = {pokemon?.name?.english} />}>Type</Link>
-          <br/><Link to={`/pokemon/${id}/name`} element={<PokemonDetail name = {pokemon?.name?.english} />}> Different Names</Link>
-  </Card> 
-          </div> ) : null}
-                          
-          </div>
-    )
+          <h1>{pokemon?.name?.english}</h1>
+          <Card
+            hoverable
+            style={{ width: 220 }}
+            cover={
+              <img
+                alt={pokemon.name.english}
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+              />
+            }
+          >
+            <Meta title={pokemon.name.english} description="" />
+            <br />
+            <Link
+              to={`/pokemon/${id}/base`}
+              element={<PokemonDetail name={pokemon?.name?.english} />}
+            >
+              Stats
+            </Link>
+            <br />
+            <Link
+              to={`/pokemon/${id}/type`}
+              element={<PokemonDetail name={pokemon?.name?.english} />}
+            >
+              Type
+            </Link>
+            <br />
+            <Link
+              to={`/pokemon/${id}/name`}
+              element={<PokemonDetail name={pokemon?.name?.english} />}
+            >
+              {" "}
+              Different Names
+            </Link>
+          </Card>
+        </div>
+      ) : null}
+    </div>
+  );
 }
