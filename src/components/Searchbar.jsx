8f1c73ba74e14d598c461f608b5c4597045ea1df
DarrellRoberts <mrdarrellroberts@gmail.com>
2023-10-10 14:58:38 +0200

    more searchbar stuff

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -4,39 +4,44 @@ import { useEffect, useState } from "react";
 const SearchBar = ({ setSearchBar }) => {
   const [inputValue, setValue] = useState("");
   const { Search } = Input;
-  const handleSearchBar = (e) => {
-    e.preventDefault();
-    setSearchBar(inputValue);
-    setValue("");
-  };
+  // const handleSearchBar = (e) => {
+  //   e.preventDefault();
+  //   setSearchBar(inputValue);
+  //   setValue("");
+  // };
 
   const capitaliseFirst = (e) => e.charAt(0).toUpperCase() + inputValue.slice(1);
+  
+  const onSearch = (e) => {
+    setSearchBar(capitaliseFirst(inputValue));
+    setValue("");
+  };
+  
   const handleInputChange = (e) => {
     setValue(e.target.value);
     setSearchBar(capitaliseFirst(e.target.value));
   };
 
-  
-
   return (
     <>
     <Space direction="vertical">
       <Search
-      onSubmit={handleSearchBar}
+      onSearch={onSearch}
       placeholder="Search for pokemon"
       allowClear
+      value={inputValue}
       enterButton="Search"
-      style={{textTransform: "capitalize"}}
       size="large"
       onChange={handleInputChange}
     />
+      </Space>
       {/* <form onSubmit={handleSearchBar}>
         <div className="input-group">
           <input type="text" value={inputValue} onChange={handleInputChange} />
           <input type="submit" value="Search" />
         </div>
       </form> */}
-    </Space> 
+  
     </>
   );
 };
