a9aba1c9c71723877e21fa2299a593885a191368
Ehsan Shahdoust <ehsan.shahdoust@gmail.com>
2023-10-12 10:56:15 +0200

    Fetched data and update page leaderboard

--- src/components/game/Leaderboard.jsx
+++ src/components/game/Leaderboard.jsx
@@ -1,129 +1,132 @@
 import { useEffect, useState } from "react";
-import Spinner from "../Spinner"
-
+import Spinner from "../Spinner";
 
 export default function Leaderboard() {
-    const [pokemon, setPokemon] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-      const timer = setTimeout(() => {
-        setIsLoading(false);
-      }, 2000);
-      return () => clearTimeout(timer);
-    }, []);
+  const [infoAll, setAllInfo] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    return (
-        <>
-        <h1 style={{color: "white", fontSize: "10rem", textAlign: "center"}}>Leaderboard</h1>
-        <div className="leaderboardContainer" style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
-        <div className="greyContainer" style={{backgroundColor: "rgba(100, 100, 100, 0.4)", display: "flex", justifyContent:"start", flexDirection: "column", alignItems: "center", borderRadius: "20px", width: "80%", minHeight: "100vh", marginBottom: "10px"}}>
-            {isLoading ? 
+  const URL = "https://pokemon-backend-ydlf.onrender.com/api/pokemon/game/save";
+  const fetchData = async () => {
+    const data = await fetch(URL);
+    const res = await data.json();
+    if (res) {
+      setAllInfo(res);
+      //   console.log(res);
+    }
+  };
+
+  const getData = () => {
+    if (infoAll.results) {
+      infoAll.results.map((info) => {
+        if (info.username) {
+          console.log(info.username);
+        }
+      });
+      console.log(infoAll);
+    }
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+      fetchData();
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <h1 style={{ color: "white", fontSize: "10rem", textAlign: "center" }}>
+        Leaderboard
+      </h1>
+      <div
+        className="leaderboardContainer"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div
+          className="greyContainer"
+          style={{
+            backgroundColor: "rgba(100, 100, 100, 0.4)",
+            display: "flex",
+            justifyContent: "start",
+            flexDirection: "column",
+            alignItems: "center",
+            borderRadius: "20px",
+            width: "80%",
+            minHeight: "100vh",
+            marginBottom: "10px",
+          }}
+        >
+          {isLoading ? (
             <Spinner />
-        : ( <>
-            <div className="firstPlace" style={{width: "75%", margin: "2%", paddingRight: "10%", borderRadius: "20px"}}>
-                    <ul className="leftUl" style={{float: "left", listStyle: "none", display: "flex", alignItems: "center", justifyContent: "start", width: "50%", padding: "0px"}}>
-                        <li style={{color: "white", textShadow: "2px 2px 3px rgb(6, 37, 211)"}}>🥇 #1 Username</li>
-                    </ul>
-                        <ul style={{float: "right", display: "inline-flex", listStyle: "none"}}>
-                        <li> Wins <br/> 5 </li>
-                        <li> Loses <br/> 5</li>
-                        <li> Points <br/> 5</li>
-                    </ul>
+          ) : (
+            <>
+              {infoAll.results &&
+                infoAll.results.map((info) => (
+                  <>
+                    <div
+                      className="firstPlace"
+                      style={{
+                        width: "75%",
+                        margin: "2%",
+                        paddingRight: "10%",
+                        borderRadius: "20px",
+                      }}
+                    >
+                      <ul
+                        className="leftUl"
+                        style={{
+                          float: "left",
+                          listStyle: "none",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "start",
+                          width: "50%",
+                          padding: "0px",
+                        }}
+                      >
+                        <li
+                          style={{
+                            color: "white",
+                            textShadow: "2px 2px 3px rgb(6, 37, 211)",
+                          }}
+                        >
+                          🥇{info.username}
+                        </li>
+                      </ul>
+                      <ul
+                        style={{
+                          float: "right",
+                          display: "inline-flex",
+                          listStyle: "none",
+                        }}
+                      >
+                        <li>
+                          {" "}
+                          Wins
+                          <br /> {info.wins}{" "}
+                        </li>
+                        <li>
+                          {" "}
+                          Loses <br /> {info.loses}
+                        </li>
+                        <li>
+                          {" "}
+                          Points <br /> {info.points}
+                        </li>
+                      </ul>
                     </div>
-                    <div className="secondPlace" style={{width: "75%", margin: "2%", paddingRight: "10%", borderRadius: "20px"}}>
-                    <ul className="leftUl" style={{float: "left", listStyle: "none", display: "flex", alignItems: "center", justifyContent: "start", width: "50%", padding: "0px"}}>
-                        <li style={{color: "white", textShadow: "2px 2px 3px rgb(6, 37, 211)"}}>🥈 #2 Username</li>
-                    </ul>
-                        <ul style={{float: "right", display: "inline-flex", listStyle: "none"}}>
-                        <li> Wins <br/> 5 </li>
-                        <li> Loses <br/> 5</li>
-                        <li> Points <br/> 5</li>
-                    </ul>
-                    </div>
-                    <div className="thirdPlace" style={{width: "75%", margin: "2%", paddingRight: "10%", borderRadius: "20px"}}>
-                    <ul className="leftUl" style={{float: "left", listStyle: "none", display: "flex", alignItems: "center", justifyContent: "start", width: "50%", padding: "0px"}}>
-                        <li style={{color: "white", textShadow: "2px 2px 3px rgb(6, 37, 211)"}}>🥉 #3 Username</li>
-                    </ul>
-                        <ul style={{float: "right", display: "inline-flex", listStyle: "none"}}>
-                        <li> Wins <br/> 5 </li>
-                        <li> Loses <br/> 5</li>
-                        <li> Points <br/> 5</li>
-                    </ul>
-                    </div>
-                    <div className="otherPlace" style={{width: "75%", margin: "2%", paddingRight: "10%", borderRadius: "20px"}}>
-                    <ul className="leftUl" style={{float: "left", listStyle: "none", display: "flex", alignItems: "center", justifyContent: "start", width: "50%", padding: "0px"}}>
-                        <li style={{}}>#4 Username</li>
-                    </ul>
-                        <ul style={{float: "right", display: "inline-flex", listStyle: "none"}}>
-                        <li> Wins <br/> 5 </li>
-                        <li> Loses <br/> 5</li>
-                        <li> Points <br/> 5</li>
-                    </ul>
-                    </div>
-                    <div className="otherPlace" style={{width: "75%", margin: "2%", paddingRight: "10%", borderRadius: "20px"}}>
-                    <ul className="leftUl" style={{float: "left", listStyle: "none", display: "flex", alignItems: "center", justifyContent: "start", width: "50%", padding: "0px"}}>
-                        <li style={{}}>#5 Username</li>
-                    </ul>
-                        <ul style={{float: "right", display: "inline-flex", listStyle: "none"}}>
-                        <li> Wins <br/> 5 </li>
-                        <li> Loses <br/> 5</li>
-                        <li> Points <br/> 5</li>
-                    </ul>
-                    </div>
-                    <div className="otherPlace" style={{width: "75%", margin: "2%", paddingRight: "10%", borderRadius: "20px"}}>
-                    <ul className="leftUl" style={{float: "left", listStyle: "none", display: "flex", alignItems: "center", justifyContent: "start", width: "50%", padding: "0px"}}>
-                        <li style={{}}>#6 Username</li>
-                    </ul>
-                        <ul style={{float: "right", display: "inline-flex", listStyle: "none"}}>
-                        <li> Wins <br/> 5 </li>
-                        <li> Loses <br/> 5</li>
-                        <li> Points <br/> 5</li>
-                    </ul>
-                    </div>
-                    <div className="otherPlace" style={{width: "75%", margin: "2%", paddingRight: "10%", borderRadius: "20px"}}>
-                    <ul className="leftUl" style={{float: "left", listStyle: "none", display: "flex", alignItems: "center", justifyContent: "start", width: "50%", padding: "0px"}}>
-                        <li style={{}}>#7 Username</li>
-                    </ul>
-                        <ul style={{float: "right", display: "inline-flex", listStyle: "none"}}>
-                        <li> Wins <br/> 5 </li>
-                        <li> Loses <br/> 5</li>
-                        <li> Points <br/> 5</li>
-                    </ul>
-                    </div>
-                    <div className="otherPlace" style={{width: "75%", margin: "2%", paddingRight: "10%", borderRadius: "20px"}}>
-                    <ul className="leftUl" style={{float: "left", listStyle: "none", display: "flex", alignItems: "center", justifyContent: "start", width: "50%", padding: "0px"}}>
-                        <li style={{}}>#8 Username</li>
-                    </ul>
-                        <ul style={{float: "right", display: "inline-flex", listStyle: "none"}}>
-                        <li> Wins <br/> 5 </li>
-                        <li> Loses <br/> 5</li>
-                        <li> Points <br/> 5</li>
-                    </ul>
-                    </div>
-                    <div className="otherPlace" style={{width: "75%", margin: "2%", paddingRight: "10%", borderRadius: "20px"}}>
-                    <ul className="leftUl" style={{float: "left", listStyle: "none", display: "flex", alignItems: "center", justifyContent: "start", width: "50%", padding: "0px"}}>
-                        <li style={{}}>#9 Username</li>
-                    </ul>
-                        <ul style={{float: "right", display: "inline-flex", listStyle: "none"}}>
-                        <li> Wins <br/> 5 </li>
-                        <li> Loses <br/> 5</li>
-                        <li> Points <br/> 5</li>
-                    </ul>
-                    </div>
-                    <div className="otherPlace" style={{width: "75%", margin: "2%", paddingRight: "10%", borderRadius: "20px"}}>
-                    <ul className="leftUl" style={{float: "left", listStyle: "none", display: "flex", alignItems: "center", justifyContent: "start", width: "50%", padding: "0px"}}>
-                        <li style={{}}>#10 Username</li>
-                    </ul>
-                        <ul style={{float: "right", display: "inline-flex", listStyle: "none"}}>
-                        <li> Wins <br/> 5 </li>
-                        <li> Loses <br/> 5</li>
-                        <li> Points <br/> 5</li>
-                    </ul>
-                    </div>
-                    </>
-                    )}
+                  </>
+                ))}
+            </>
+          )}
         </div>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
